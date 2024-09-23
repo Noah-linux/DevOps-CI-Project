f4647c2b27834c6e68f028436bcd95983e95d4a2
Noah-linux <noahdesisa@gmail.com>
2024-09-23 12:40:44 +0000

    new

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:9090'
   },
 
   viewportHeight: 720,
